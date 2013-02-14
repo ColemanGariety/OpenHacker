cb74dcad7f5d0538aad7314a5700a72249db63ac
ColbyAley <colby@aley.me>
2013-02-13 20:04:05 -0800

    adds rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheChallenge::Application.config.secret_token = 'cb4c52206476446618d930fa52c2fac207b061f0e096980d6741bfa2f4c860cf443bf52190ea3a2c73d1f191ce8baa110c01d9bc1ccdab3a34943d523ec5e2bc'
