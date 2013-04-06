db4dc594cc6e679b4aea386528de2a6fb7fd18b0
Jackson Gariety <personal@jacksongariety.com>
2013-04-06 01:33:31 -0700

    Adds pretty half-baked submit page. Each API call works except for the last one, seems to be a bug in the GitHub API.

--- app/assets/javascripts/submit.js
+++ app/assets/javascripts/submit.js
@@ -1,13 +1,105 @@
-params = {
-  url: "https://api.github.com/user/repos?access_token=" + $("#user").attr("data-token"),
+var repos,
+  contents,
+  screenshot_jpg,
+  collaborators,
+  selected_repo,
+  found_screenshot_jpg = false,
+  found_description = false,
+  found_commits = false,
+  found_contributors = false
+
+// Get repos
+$.ajax({
+  url: "https://api.github.com/user/repos?type=owner&sort=created&access_token=" + $("#user").attr("data-token"),
   type: "GET",
   dataType: "JSONP",
   success: function(data) {
-    console.log(data)
-    for (var i = 0; i < data.data.length; i++) {
-      $("#repos").prepend("<li>" + data.data[i].name + "</li>")
+    repos = data.data
+    console.log(repos, "Repos")
+    for (var i = 0; i < repos.length; i++) {
+      $("#repos select").append("<option value=\"" + data.data[i].name + "\">" + data.data[i].name + "</option>")
     }
   }
-}
+})
 
-$.ajax(params)
+$("#repos select").change(function() {
+  if ($("#repos select").find(":selected").val() != "null") {
+    $("#step-1").removeClass("focused")
+    $("#step-2").addClass("focused")
+
+    // Start check with screenshot
+    $.ajax({
+      url: "https://api.github.com/repos/" + $("#user").attr("data-username") + "/" + $("#repos select").find(":selected").val() + "/contents/?access_token=" + $("#user").attr("data-token"),
+      type: "GET",
+      dataType: "JSONP",
+      success: function(data) {
+        contents = data.data
+
+        console.log(contents, "Contents")
+        for (var i = 0; i < contents.length; i++) {
+          if (data.data[i].name == "screenshot.jpg") {
+            found_screenshot_jpg = data.data[i].url
+          }
+        }
+
+        if (found_screenshot_jpg != false) {
+          $("#screenshot_check").find(".mark").addClass("done")
+        } else {
+          $("#screenshot_check").find(".mark").html("&#10006;").addClass("red").addClass("done")
+        }
+
+        setTimeout(function(){
+          // Check for description
+          for (var i = 0; i < repos.length; i++) {
+            if (repos[i].name == $("#repos select").find(":selected").val()) {
+              selected_repo = repos[i]
+
+              if (repos[i].description != "") {
+                found_description = repos[i].description
+              }
+            }
+          }
+
+          if (found_description != false) {
+            $("#description_check").find(".mark").addClass("done")
+          } else {
+            $("#description_check").find(".mark").html("&#10006;").addClass("red").addClass("done")
+          }
+
+          setTimeout(function(){
+            // Check initial commit
+            var opened_at = new Date($("#challenge").attr("data-opened-at"))
+            var created_at = new Date(selected_repo.created_at)
+
+            if (created_at >= opened_at) {
+              found_commits = String(created_at)
+
+              $("#commit_date_check").find(".mark").addClass("done")
+            } else {
+              $("#commit_date_check").find(".mark").html("&#10006;").addClass("red").addClass("done")
+            }
+
+            setTimeout(function(){
+              // Check contributors
+              $.ajax({
+                url: "https://api.github.com/repos/" + $("#user").attr("data-username") + "/" + $("#repos select").find(":selected").val() + "/collaborators/?access_token=" + $("#user").attr("data-token"),
+                type: "GET",
+                dataType: "JSONP",
+                success: function(data) {
+                  console.log(data, "Collaborators")
+
+                  if (data.data.message != "Not Found") {
+                    $("#contributor_check").find(".mark").addClass("done")
+                  } else {
+                    $("#contributor_check").find(".mark").html("&#10006;").addClass("red").addClass("done")
+                  }
+                }
+              })
+            },500)
+          },500)
+
+        },500)
+      }
+    })
+  }
+});
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,5 @@
 class Entry < ActiveRecord::Base
-  attr_accessible :description, :receiving_challenge_id, :repo_url, :submitting_user_id, :thumb_url, :title
+  attr_accessible :description, :receiving_challenge_id, :repo_url, :submitting_user_id, :thumb_url, :title, :demo_url
 
   belongs_to :user
   belongs_to :challenge
