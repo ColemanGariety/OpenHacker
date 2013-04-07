4804120819f033be55ceff3a08b1f8809109d2d9
ColbyAley <colby@aley.me>
2013-04-06 20:50:03 -0700

    Fixes votes so that they update when you vote.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,7 +2,9 @@ class VotesController < ApplicationController
   # POST /votes
   # POST /votes.json
   def vote
-    @vote = Vote.find_or_initialize_by_value_and_receiving_entry_id_and_voting_user_id(params[:value], params[:receiving_entry_id], current_user.id)
+    @vote = Vote.find_or_initialize_by_receiving_entry_id_and_voting_user_id(params[:receiving_entry_id], current_user.id)
+    
+    @vote.value = params[:value]
 
     respond_to do |format|
       if @vote.save
