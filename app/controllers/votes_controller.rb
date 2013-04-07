9972cc5641b89decc54a52fea00403b33a3eb222
ColbyAley <colby@aley.me>
2013-04-06 20:16:10 -0700

    Adds voting and stars. Doesn't save value though.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,31 +1,13 @@
 class VotesController < ApplicationController
   # POST /votes
   # POST /votes.json
-  def create
-    @vote = Vote.new(params[:vote])
+  def vote
+    @vote = Vote.find_or_initialize_by_receiving_entry_id_and_voting_user_id(params[:receiving_entry_id], current_user.id)
 
     respond_to do |format|
       if @vote.save
-        format.html { redirect_to @vote, notice: 'Vote was successfully created.' }
         format.json { render json: @vote, status: :created, location: @vote }
       else
-        format.html { render action: "new" }
-        format.json { render json: @vote.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /votes/1
-  # PUT /votes/1.json
-  def update
-    @vote = Vote.find(params[:id])
-
-    respond_to do |format|
-      if @vote.update_attributes(params[:vote])
-        format.html { redirect_to @vote, notice: 'Vote was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
         format.json { render json: @vote.errors, status: :unprocessable_entity }
       end
     end
