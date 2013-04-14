d19e1b0b08a8fcb039a5aeb0041ee39c909df9bc
ColbyAley <colby@aley.me>
2013-04-13 18:22:13 -0700

    Adds top page, yet to be stiled

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,20 @@
 ThechallengeIo::Application.routes.draw do
   root :to => "entries#index"
+  
 
   resources :challenges
   resources :entries
   resources :users
   resources :votes
 
-  get "suggestions" => "challenges#suggestions", :as => "challenge_suggestions"
-
-  match "submit" => "entries#new", :as => "submit"
+  get 'suggestions' => 'challenges#suggestions', :as => 'challenge_suggestions'
+  match 'top' => 'entries#top', :as => 'top'
+  match 'submit' => "entries#new", :as => 'submit'
   match 'auth/github/callback' => 'sessions#create'
-  match "auth/failure" => redirect("/")
-  match 'logout' => 'sessions#destroy', :as => "signout"
+  match 'auth/failure' => redirect("/")
+  match 'logout' => 'sessions#destroy', :as => 'signout'
   match 'about' => 'pages#about'
   match 'rules' => 'pages#rules'
   
-  put "vote", :to => "votes#vote", :as => :vote
+  put 'votes', :to => 'votes#vote', :as => :vote
 end
