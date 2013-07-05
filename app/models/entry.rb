ccf538a2dd2b9b4d37aacdd55940f8ce88213d03
Colby Aley <colby@aley.me>
2013-07-04 23:38:33 -0700

    Validates presence of fields upon entry submission

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -5,4 +5,13 @@ class Entry < ActiveRecord::Base
   has_many :votes
   
   validates_uniqueness_of :github_repo_id
+  validates_uniqueness_of :demo_url
+  validates_uniqueness_of :repo_url
+  validates_presence_of :title
+  validates_presence_of :description
+  validates_presence_of :repo_url
+  validates_presence_of :thumb_url
+  validates_presence_of :demo_url
+  validates_presence_of :github_repo_id
+
 end
